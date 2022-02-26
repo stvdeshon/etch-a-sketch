848cf5ebc943ade9049a1cae153f377561fa2dc8
stvdeshon <stvdeshon@gmail.com>
2022-02-26 12:46:39 -0700

    Commit the first iteration of the etch-a-sketch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+This is my etch-a-sketch project following the Odin Project curriculum. It's a work in progress as I update the UI having finished the basic structure of the project. 
--- sketch.js
+++ sketch.js
@@ -1,26 +1,38 @@
 const container = document.querySelector('#container');
+let base = 16;
 
 
 function sketchPad(rows) {
    for (let i = 0; i < rows; i++) {
-     let row = document.createElement('div');
+     let col = document.createElement('div');
+     col.style.setProperty('--grid-cols', rows)
     //  container.appendChild(row).className = 'row';
      for (let j = 0; j < rows; j++) {
-       let col = document.createElement('div');
-       row.appendChild(col).className = 'col';
+       let row = document.createElement('div');
+       col.appendChild(row).className = 'row';
      }
-     container.appendChild(row).className = 'row';
-   }
-  
-   const boxy = document.querySelectorAll('.row');
-   boxy.forEach((box) => {
-    box.addEventListener('click', () => {
-      box.style.backgroundColor = 'black';
-    })
-  })
+     container.appendChild(col).className = 'col';
 
+   }
+
+  
+   const column = document.querySelectorAll('.row');
+   let mouseIsDown = false;
+   column.forEach((box) => {
+      box.addEventListener('mousedown', function(){mouseIsDown = true});
+      box.addEventListener('mouseup', function(){mouseIsDown = false});
+      box.addEventListener('mousemove', function(){
+        if(mouseIsDown){
+          box.style.backgroundColor = 'black';   
+        };
+      });
+    });
 }
 
 
 
-sketchPad(16);
+
+
+
+
+sketchPad(64);
